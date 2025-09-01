1287e98e5d9ae5a64192b59635ae51491f760a77
Ayotomiwa <bamisaye.tomiwa@yahoo.com>
2025-09-01 14:41:17 +0100

    Apply Terraform formatting fixes

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     dynatrace = {
-      source = "dynatrace-oss/dynatrace"
+      source  = "dynatrace-oss/dynatrace"
       version = ">= 1.77.0"
     }
   }
 }
 
 provider "dynatrace" {
-  dt_env_url = var.dt_env_url
+  dt_env_url   = var.dt_env_url
   dt_api_token = var.dt_api_token
 }
 
