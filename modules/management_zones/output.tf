7afaa40aa9b780b731d87530d475097b819da92b
Ayotomiwa <bamisaye.tomiwa@yahoo.com>
2025-09-08 17:26:12 +0100

    Updated Terraform modules config

--- modules/management_zones/output.tf
+++ modules/management_zones/output.tf
@@ -1,3 +1,4 @@
+# Map of Management Zone IDs keyed by logical keys
 output "management_zone_ids" {
   description = "Map of MZ IDs keyed by logical key."
   value       = { for k, r in dynatrace_management_zone_v2.mz : k => r.id }
--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
+# Exposes Management Zone IDs keyed by logical keys
 output "management_zone_ids" {
   value = module.management_zones.management_zone_ids
 }
