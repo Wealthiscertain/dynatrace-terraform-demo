b001be27bec0aa9fd9fdf7ecf837284326ec7910
Ayotomiwa <bamisaye.tomiwa@yahoo.com>
2025-09-08 17:14:42 +0100

    Updated terraform module config

--- modules/management_zones/output.tf
+++
@@ -1,4 +0,0 @@
-output "management_zone_ids" {
-  description = "Map of created/imported management zone resource IDs keyed by mz key."
-  value       = { for k, r in dynatrace_management_zone_v2.mz : k => r.id }
-}
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "management_zone_ids" {
+  value = module.management_zones.management_zone_ids
+}
