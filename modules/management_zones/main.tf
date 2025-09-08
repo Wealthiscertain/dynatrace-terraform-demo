261248891da222306506bb3e73ec06e0be68181f
Ayotomiwa <bamisaye.tomiwa@yahoo.com>
2025-09-08 16:35:28 +0100

    Terraform module updates

--- modules/management_zones/main.tf
+++ modules/management_zones/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    dynatrace = {
-      source  = "dynatrace-oss/dynatrace"
-      version = ">= 1.77.0"
-    }
-  }
-}
-
 # One resource per item in mz_names
 resource "dynatrace_management_zone_v2" "mz" {
   for_each = var.mz_names
--- modules/openpipeline/main.tf
+++
@@ -1,9 +0,0 @@
-# resource "dynatrace_openpipeline_business_events" "demo_pipeline" {
-#   pipelines {
-#     pipeline {
-#       id           = "terraform_demo_pipeline"
-#       enabled      = true
-#       display_name = "Terraform Demo Pipeline"
-#     }
-#   }
-# }
